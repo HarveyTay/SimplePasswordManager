a9192b4c2e201c616085c3540995f976d45b55e0
Harvey <harveytaylor457@gmail.com>
2025-05-15 20:23:36 +0100

    created saving functionality and loading the whole database

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include <fstream>
 #include <iostream>
 #include <vector>
+#include <sstream>
 
 struct Credential
 {
@@ -22,13 +23,33 @@ void loadCredentialDatabase()
     }
 }
 
+
+void saveCredentialsToDatabase(std::vector<Credential>& creds)
+{
+    try
+    {
+        std::ofstream vaultFile("vault.csv", std::ios::trunc);
+
+        for (const auto& [service, username, password] : creds) {
+            vaultFile << service << "," << username << "," << password << "\n";
+        }
+
+
+        vaultFile.close();
+    }
+    catch (...)
+    {
+        std::cout << "Credentials Couldn't be saved";
+    }
+}
+
 void addCredentialEntryToMem(std::vector<Credential>& creds)
 {
     try
     {
         Credential cred;
         std::cout << "What are these credentials for?";
-        std::getline(std::cin, cred.service);
+        std::getline(std::cin, cred.service);//update service
 
         std::cout << "Whats your username for this service?";
         std::getline(std::cin, cred.username);
@@ -37,17 +58,38 @@ void addCredentialEntryToMem(std::vector<Credential>& creds)
         std::getline(std::cin, cred.password);
 
         creds.push_back(cred);
+
+        saveCredentialsToDatabase(creds);
     }
     catch(...)
     {
-        std::cout << "Credentials Couldn't Be Appended"; // catch errors
+        std::cout << "Credential Update Error"; // catch errors
+    }
+
+}
+
+void loadCredentialDatabase(std::vector<Credential>& creds)
+{
+    std::ifstream vaultFile("vault.csv");
+    std::string currLine;
+
+    while (std::getline(vaultFile, currLine)){
+        std::stringstream ss(currLine);
+
+        Credential cred;
+
+        std::getline(ss, cred.service, ',');
+        std::getline(ss, cred.username, ',');
+        std::getline(ss, cred.password, ',');
+
+        creds.push_back(cred);
     }
 
 }
 
 int main()
 {
-    loadCredentialDatabase();
+    std::vector<Credential> creds;
 
     int sel;
     do
@@ -59,10 +101,11 @@ int main()
         std::cin >> sel;
         switch (sel)
             {
-            //case 1: (); break;
+            case 1: addCredentialEntryToMem(creds); break;
+            case 2: loadCredentialDatabase(creds); break;
             default: std::cout << "Choose a valid option\n";
             }
-    } while (sel !=1&&sel!=2&&sel!=3);
+    } while (sel!=3);
 
     return 0;
 }
