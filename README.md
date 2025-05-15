3cf524ae81a95a287e3e355e1b78f097b75781f9
Harvey <harveytaylor457@gmail.com>
2025-05-15 17:28:35 +0100

    Create the DB csv file if not found, update the Credential struct with user input for service, username and password and added formatting for the main program using do-while and switch statement

---
+++ README.md
@@ -0,0 +1,12 @@
+# Simple Password Manager
+
+
+
+---
+
+## To Do:
+    Add encryption
+    make gui
+    change database type(csv/db)
+    create password generator
+    
--- main.cpp
+++ main.cpp
@@ -1,28 +1,68 @@
 #include <fstream>
 #include <iostream>
+#include <vector>
 
-void CreatePasswordDatabase()
+struct Credential
 {
-    std::ofstream passwordDatabase("passwords.txt");
+    std::string service;
+    std::string username;
+    std::string password;
+};
+
+
+void loadCredentialDatabase()
+{
+    std::fstream vaultFile;
+    vaultFile.open("vault.csv");
+    if (!vaultFile.is_open())
+    {
+        std::ofstream Database("vault.csv"); //create the database
+        Database << "service,username,password\n";
+        Database.close();
+    }
 }
 
-void AddPasswordToDatabase(const std::string& Credential)
+void addCredentialEntryToMem(std::vector<Credential>& creds)
 {
     try
     {
-        std::ifstream passwordDatabase("passwords.txt");
-        passwordDatabase.open("passwords.txt");
+        Credential cred;
+        std::cout << "What are these credentials for?";
+        std::getline(std::cin, cred.service);
 
-        passwordDatabase << (operator<<(std::cout, Credential), "\n");
+        std::cout << "Whats your username for this service?";
+        std::getline(std::cin, cred.username);
+
+        std::cout << "Whats your password for this service?";
+        std::getline(std::cin, cred.password);
+
+        creds.push_back(cred);
     }
     catch(...)
     {
-        std::cout << "Credentials Couldn't Be Appended";
+        std::cout << "Credentials Couldn't Be Appended"; // catch errors
     }
 
 }
 
 int main()
 {
+    loadCredentialDatabase();
 
+    int sel;
+    do
+    {
+        std::cout << "1. Add Credential\n";
+        std::cout << "2. View Credentials\n";
+        std::cout << "3. Save and Exit\n";
+        std::cout << "Select an option: ";
+        std::cin >> sel;
+        switch (sel)
+            {
+            //case 1: (); break;
+            default: std::cout << "Choose a valid option\n";
+            }
+    } while (sel !=1&&sel!=2&&sel!=3);
+
+    return 0;
 }
