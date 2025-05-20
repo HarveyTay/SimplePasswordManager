cc85244c503e3adc8c26aeeef2dba1f52ab346e7
Harvey <harveytaylor457@gmail.com>
2025-05-20 22:45:58 +0100

    changed the functions to use string& for more efficiency

--- vault.h
+++ vault.h
@@ -7,9 +7,9 @@
 #include "credential.h"
 
 void createCredentialDatabase();
-void saveCredentialsToDatabase(std::vector<Credential>& creds, std::string key);
+void saveCredentialsToDatabase(std::vector<Credential>& creds, std::string& key);
 void addCredentialEntryToMem(std::vector<Credential>& creds);
-void loadCredentialDatabase(std::vector<Credential>& creds, std::string key);
+void loadCredentialDatabase(std::vector<Credential>& creds, std::string& key);
 void viewCredentials(std::vector<Credential>& creds);
 std::string xorEncryptionDecryption(const std::string& data, const std::string& key);
 
